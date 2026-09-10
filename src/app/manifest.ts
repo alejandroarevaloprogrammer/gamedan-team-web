36d381b4e9680ef748b7011d2ab6b85cc71fae75
Alejandro Arevalo Rojas <alejandroarevalo.programmer@gmail.com>
2026-09-10 08:59:57 +0200

    Organize public icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,25 +15,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#FBF5A7",
     icons: [
       {
-        src: "/android-chrome-192x192.png",
+        src: "/icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/android-chrome-512x512.png",
+        src: "/icons/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/android-chrome-192x192-maskable.png",
+        src: "/icons/android-chrome-192x192-maskable.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/android-chrome-512x512-maskable.png",
+        src: "/icons/android-chrome-512x512-maskable.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
